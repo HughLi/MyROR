538f2bf20e15c5987edae1de94afc889e45ff91e
ex-liyue003 <ex-liyue003@pingan.com.cn>
2018-01-23 20:54:37 +0800

    play app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html:"hello world"
+    render html:"我们幼儿园里还有个小朋友叫李紫莯,她今天没到幼儿园,哦好像就是我呀"
   end
 end
