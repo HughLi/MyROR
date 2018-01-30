b356dd8d35acce1c32a234dfa56d581ca02a3af8
ex-liyue003 <ex-liyue003@pingan.com.cn>
2018-01-30 21:52:37 +0800

    finish toy App

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many:microposts
+    validates :name,presence: true
+
+    
+    # validates :content, length: {maximum: 140} ,presence :true
+    
+    validates :email,presence: true
+
 end
